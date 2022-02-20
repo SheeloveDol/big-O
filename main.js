13098271159feca451e1b7c3b93a49f1f7a6aa08
Sheelove Dol <siesel26@gmail.com>
2022-02-20 11:34:09 -0800

    A simple exercise to practive bigO and readability of a function

--- main.js
+++ main.js
@@ -23,3 +23,43 @@ function findNemo(array) {
 }
 
 findNemo(casts) 
+
+console.log('--------------')
+
+
+// ---------- Interview question ------------
+// Create a function that reverses a string
+
+// my brute approach: 
+
+// creating function with a function declaration
+function reverseString(str) {
+  // splitting the string into an array and assigning the result to an array
+  let strToArray = str.split('')
+
+  // creating an empty array to store the reverse of strToArray
+  let reversedArray = [];
+
+  // looping through the strToArray and pushing its items into reversedArray in reverse
+  for (let i = strToArray.length -1; i >= 0; i--) {
+    reversedArray.push(strToArray[i])
+  }
+  
+  // turing reversedArray into a string and storing into a new variable
+  let backwardString = reversedArray.join('')
+  
+
+  return backwardString;
+  
+}
+// This above function has a time complexity of O(n)
+
+console.log(reverseString('Sheelove is getting really good!')) 
+
+
+
+// The above is ,of course, the brute approach and also an attempt to be as verbose as possible. I can refactor it to be more concise using built-in methods.
+
+const conciseStringReversal = (string) => string.split('').reverse().join('');
+
+console.log(conciseStringReversal('Is this the right thing?'))
